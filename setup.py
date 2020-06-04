cbbbd68847a35fabb974865a8cd05033a04cde80
neoul <neoul@ymail.com>
2020-06-04 20:02:22 +0900

    Increased the version of pyangexcel

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='pyangexcel',
-    version='0.2',
+    version='0.3',
     description=('A pyang excel plugin to produce a Excel Schema file'),
     long_description=read('README.md'),
     long_description_content_type="text/markdown",
